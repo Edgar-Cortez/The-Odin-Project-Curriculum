6e35855a7242231e4099e6050001cdf678e084fa
EdgarCortez1 <ea.cortezz@gmail.com>
2019-11-26 10:49:48 -0600

    Completed chapter

---
+++ LearnToProgramBook/Chapter01/01-calc.rb
@@ -0,0 +1,70 @@
+# Intro to puts
+puts 1 + 2
+
+# Output Below
+# 3
+
+
+# ***********************************************************
+
+# Simple Arithmetic
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# Output Below
+# 3.0
+# 6.0
+# -3.0
+# 4.5
+
+
+
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+# Output Below
+# 3
+# 6
+# -3
+# 4			Truncates the decimal and rounds the result down
+
+
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
+
+# Output Below
+# 5
+# -29802
+
+# ***********************************************************
+
+# Try Section
+
+# How many hours in a year?
+puts 365 * 24
+
+# Output Below
+# 8760
+
+# How many minutes are in a decade?
+puts 10 * (365 * (24 * 60))
+
+# Output Below
+# 525600
+
+# How many seconds old are you?
+puts (((30 * 365) * 24) * 60) * 60
+
+# Output Below
+# 946080000
+
+# If I am 1298 million seconds old, how old am I (in years)?
+puts ((((1298000000 / 60) / 60) / 24) / 365)
+
+# Output Below
+41
